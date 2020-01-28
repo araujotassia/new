8f1b8b858328a5dcf7f3e825d67c0f7c1b3a2718
Tassia Araujo <araujot@vanier.college>
2020-01-28 11:12:59 -0500

    Added sandbox

---
+++ Sandbox.java
@@ -0,0 +1,42 @@
+/*
+    My comment goes here
+
+dhdth
+fgjfgj
+fghjfgj
+dtjtdjd
+
+    My comment here
+
+*/
+
+
+public class Sandbox {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        // Second line of comment
+        // cfhc
+        System.out.println("\nHello, World"+"\nTassia "+"and the rest of the world!!!!");
+
+        System.out.println("\nSecond statement");
+
+        final double PI = 3.14;
+
+        double radius = 5;
+        double area;
+
+        area = PI * radius * radius;
+
+        System.out.println("The area of the circle is " + area);
+
+        int currentYear = 2020;
+        int yearOfBirth = 1983;
+
+        int age = currentYear - yearOfBirth;
+
+        System.out.println("My age is " + age);
+
+    }
+
+}
